20541eb9365425f382381afdbd4494961a2babc3
agaertner <13819164+agaertner@users.noreply.github.com>
2023-01-26 20:08:10 +0100

    ⬆️ Upgrade bhud to stable v1.0.0

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TemplateModule")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A template module for Blish-HUD.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TemplateModule")]
